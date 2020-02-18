966dbbbe909d595a7187350e7b2b82dbef28dd52
Wojciech <olowek92@gmail.com>
2020-02-18 21:24:16 +0100

    Task-2 of a day done

--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,15 @@
-//Twój kod
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("Message from back-end!");
+
+  const userAgent = req.headers["user-agent"];
+
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+
+  res.end(`<h1>${userAgent}</h1>`);
+});
+
+server.listen(3300, () => {
+  console.log("Serwer uruchomiony na porcie 3300");
+});
