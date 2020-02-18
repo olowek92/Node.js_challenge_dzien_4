1797ba9af594bb057bce81a8ed614ad401b120f9
Wojciech <olowek92@gmail.com>
2020-02-18 21:04:18 +0100

    Task-1 of a day done

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,11 @@
-//Twój kod
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+  response.setHeader("Content-Type", "text/html; charset=utf-8");
+
+  response.end(`<h1>Hello,World from back-end!</h1>`);
+});
+
+server.listen(4000, () => {
+  console.log("Hello, World from back-end!");
+});
